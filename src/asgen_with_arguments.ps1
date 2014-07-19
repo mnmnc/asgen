4fb6522e22e06f0a5d8b6740807dd6449fe7b8b6
mnmnc <chooseright+git@gmail.com>
2014-07-19 11:16:47 +0200

    Version that captures the command line arguments for the processes.

---
+++ src/asgen_with_arguments.ps1
@@ -0,0 +1,59 @@
+###############################
+# AUTOMATIC STARTUP GENERATOR #
+# ASGEN v.1.0 by mnmnc @ 2014 # 
+###############################
+
+# VARIABLES
+
+	# CURRENT PATH
+	$current = $(pwd).ToString()
+
+	# DESTINATION PATH
+	$destination_path = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\programs.bat"
+
+# MOVING TO TEMP	
+cd $env:systemroot\Temp
+
+# REMOVING OLD FILE
+del $destination_path
+
+# EMPTY ARRAYS
+$arr_names = @()
+$arr_paths = @()
+
+# FILLING ARRAYS
+Get-WmiObject Win32_Process | Where-Object { $_.Path -match "Program" } | foreach { $arr_names += $_.Name; $arr_paths += $_.CommandLine;  }
+#Get-Process | Where-Object {$_.Path -match "Program" } | foreach { $arr_names += $_.Name; $arr_paths += $_.Path }
+
+# CREATING FILE
+for($i = 0; $i -le $arr_names.count -1; $i++) 
+{ 
+	$out =  $arr_paths[$i] 
+	write-output $out | out-file "programs.bat" -append -Encoding ascii
+}
+
+# INFO
+Write-host "`nFolowing programs will be started on next boot:`n"
+cat programs.bat
+Write-host "`n"
+
+# MOVING TO STARTUP FOLDER
+mv programs.bat "$destination_path"
+
+# RETURNING
+cd $current
+
+exit
+
+
+
+
+
+
+
+
+
+
+
+
+
